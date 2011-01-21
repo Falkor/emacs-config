5bfbe21bb2321a25c3ec9bdf1fbd9393208ecf3f
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2011-01-21 10:40:45 +0100

    New custom made for Aquamacs support
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- custom.el
+++ custom.el
@@ -3,12 +3,25 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 210 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((tool-bar-lines . 1) (menu-bar-lines . 1) (cursor-color . "green") (cursor-type . box))))
+ '(display-time-mode t)
+ '(ecb-fix-window-size (quote auto))
  '(ecb-kill-buffer-clears-history (quote auto))
- '(ecb-layout-name "falkor")
+ '(ecb-layout-name "falkor1")
  '(ecb-options-version "2.40")
  '(ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
+ '(ecb-show-sources-in-directories-buffer (quote ("falkor1" "left7" "left13" "left14" "left15")))
  '(ecb-source-file-regexps (quote ((".*" ("\\(^\\(\\.\\|#\\)\\|\\(~$\\|\\.\\(elc\\|obj\\|o\\|class\\|lib\\|dll\\|a\\|so\\|cache\\)$\\)\\)") ("^\\.\\(emacs\\|gnus\\)$")))))
- '(ecb-source-path (quote (("~/" "Home") (#("/Users/svarrette/svn/gforge.uni.lu/inids/trunk" 0 46 (help-echo "Mouse-2 toggles maximizing, mouse-3 displays a popup-menu")) "LibINIDS") (#("/Users/svarrette/svn/gforge.uni.lu/libgep" 0 41 (help-echo "Mouse-2 toggles maximizing, mouse-3 displays a popup-menu")) "LibGEP")))))
+ '(ecb-source-path (quote (("~/" "Home") (#("/Users/svarrette/svn/gforge.uni.lu/inids/trunk" 0 46 (help-echo "Mouse-2 toggles maximizing, mouse-3 displays a popup-menu")) "LibINIDS") (#("/Users/svarrette/svn/gforge.uni.lu/libgep" 0 41 (help-echo "Mouse-2 toggles maximizing, mouse-3 displays a popup-menu")) "LibGEP") ("/Users/svarrette/git/github.com/Falkor" "Github/Falkor") ("/Users/svarrette/git/github.com/Falkor/emacs-config" "Github/Falkor/emacs-config") ("/Users/svarrette/git/github.com/Falkor/yabackup" "Github/Falkor/yabackup") ("/Users/svarrette/git/github.com/Falkor/dotfiles" "Github/Falkor/dotfiles") ("/Users/svarrette/git/grid5000.fr/puppet-repo" "G5K/Puppet") (#("/Users/svarrette/devel/www/WebGen" 0 33 (help-echo "Mouse-2 toggles maximizing, mouse-3 displays a popup-menu")) "WebGen"))))
+ '(ecb-windows-width 35)
+ '(ns-alternate-modifier nil)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(visual-line-mode nil t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
